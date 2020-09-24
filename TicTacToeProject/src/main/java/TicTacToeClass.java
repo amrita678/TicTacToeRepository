095caabf3b56dc84b385c338e3b456fbf6c20213
AMRITA <amritagolu678@gmail.com>
2020-09-24 12:26:19 +0530

    Should print empty board

--- TicTacToeProject/src/main/java/TicTacToeClass.java
+++ TicTacToeProject/src/main/java/TicTacToeClass.java
@@ -2,6 +2,14 @@
 public class TicTacToeClass {
 
 	public String printEmptyBoard() {
-    	 return null;
-	}
+		String board ="/---|---|---\\"
+			    +"\n" + "| 1 | 2 | 3 |"
+			    +"\n"  +"|-----------|"
+			    + "\n"+ "| 4 | 5 | 6 |"
+			     +"\n"+ "|-----------|" 
+			     +"\n"+ "| 7 | 8 | 9 |"
+			     +"\n"+ "|-----------|";
+			
+	return board;
+}
 }
